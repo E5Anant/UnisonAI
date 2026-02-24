fca8a00f26c42897e04a618c47e0641208edb589
Anant Sharma <e5anant2011@gmail.com>
2026-02-24 16:47:36 +0530

    refactor: clean up __init__.py exports, remove config references

--- unisonai/__init__.py
+++ unisonai/__init__.py
@@ -2,6 +2,5 @@ from .agent import Agent
 from .clan import Clan
 from .tools.tool import Field, BaseTool
 from .tools.types import ToolParameterType
-from .config import config
 
-__all__ = ['Agent', 'config', 'BaseTool', 'Field', 'Clan']
+__all__ = ['Agent', 'BaseTool', 'Field', 'Clan']
